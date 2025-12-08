76a795e0ca2715c565752a7d1b0f1953eefe2252
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-12-09 00:08:07 +0300

    fix: constants

---
+++ packages/client/src/constants.ts
@@ -0,0 +1,14 @@
+export const options = [
+  { value: '1', content: 'January' },
+  { value: '2', content: 'February' },
+  { value: '3', content: 'March' },
+  { value: '4', content: 'April' },
+  { value: '5', content: 'May' },
+  { value: '6', content: 'June' },
+  { value: '7', content: 'July' },
+  { value: '8', content: 'August' },
+  { value: '9', content: 'September' },
+  { value: '10', content: 'October' },
+  { value: '11', content: 'November' },
+  { value: '12', content: 'December' },
+];
