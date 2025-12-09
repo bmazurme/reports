9fc5d2ed9db376f10229f13d0da36ce1863d7d2e
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-12-09 23:55:57 +0300

    fix: types

--- packages/shared/src/types.ts
+++ packages/shared/src/types.ts
@@ -7,3 +7,54 @@ export interface StreamEvent {
   type: StreamEventType;
   data?: any;
 }
+
+export type KeyType = 'allDays' | 'holidays' | 'weekends' | 'offDays' | 'shortDays' | 'workDays' | 'hours';
+export type MonthType = {
+  allDays: number;
+  holidays: number;
+  weekends: number;
+  offDays: number;
+  shortDays: number;
+  workDays: number;
+  hours: number;
+}
+
+export type MonthKeyType = '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | '11' | '12';
+
+export type DateType = {
+  calendar: Record<MonthKeyType, MonthType>;
+  holidays: string[];
+  shortDays: string[];
+  badDays: string[];
+  offDays: string[];
+}
+
+export type UserType = {
+  id: number;
+  username: string;
+}
+
+
+export type ResType = {
+  id: string;
+  iid: string;
+  title: string;
+  project_id: number;
+  state: string;
+  created_at: string;
+  time_stats?: {
+    human_time_estimate: string;
+  },
+}
+
+export type IssueResponse = ResType[];
+
+export type ResultType = {
+  id: string;
+  iid: string;
+  title: string;
+  project: number;
+  status: string;
+  created: string;
+  timeStats: string;
+}
