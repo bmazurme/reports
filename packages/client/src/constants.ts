bdf4b5729e3dee8ccac8c618d6ecc4a383018b25
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-12-10 14:56:34 +0300

    fix: constants

--- packages/client/src/constants.ts
+++ packages/client/src/constants.ts
@@ -1,3 +1,7 @@
+import { TableColumnConfig } from '@gravity-ui/uikit';
+import type { KeyType } from '@reports/shared';
+import { RowData } from './hocs/with-table-sorting';
+
 export const options = [
   { value: '1', content: 'January' },
   { value: '2', content: 'February' },
@@ -11,4 +15,24 @@ export const options = [
   { value: '10', content: 'October' },
   { value: '11', content: 'November' },
   { value: '12', content: 'December' },
+];
+export const years = [
+  { value: '2025', content: '2025' },
+  { value: '2026', content: '2026' },
+];
+
+export const fields: Record<KeyType, string> = {
+  allDays: 'All days',
+  holidays: 'Holidays',
+  weekends: 'Weekends',
+  offDays: 'Off days',
+  shortDays: 'Short days',
+  workDays: 'workDays',
+  hours: 'Hours'
+};
+
+export const columns: TableColumnConfig<RowData>[] = [
+  { id: 'name', name: 'Наименование задачи', align: 'start', meta: { sort: true } },
+  { id: 'status', name: 'Статус задачи', align: 'center', meta: { sort: true } },
+  { id: 'time', name: 'Часы', align: 'center', meta: { sort: true } },
 ];
