0880b35d56e98a93b411010397ec6035b8f3978d
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-12-10 14:56:58 +0300

    fix: app

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,29 +1,32 @@
 import { useEffect, useState } from 'react';
 import { Calendar } from '@gravity-ui/date-components';
 import { dateTimeParse, type DateTime } from '@gravity-ui/date-utils';
-import { TextInput, Text, Select, Loader, TableColumnConfig } from '@gravity-ui/uikit';
+import { TextInput, Text, Select, Loader } from '@gravity-ui/uikit';
 import type { DateType, MonthKeyType, KeyType } from '@reports/shared';
-import { options } from './constants';
+import { options, fields, columns, years } from './constants';
 import MyTable, { RowData } from './hocs/with-table-sorting';
+
+import fetchDataFromApi from './api';
 
 import './App.css';
 import style from './app.module.css';
 
-const fields: Record<KeyType, string> = {
-  allDays: 'All days',
-  holidays: 'Holidays',
-  weekends: 'Weekends',
-  offDays: 'Off days',
-  shortDays: 'Short days',
-  workDays: 'workDays',
-  hours: 'Hours'
+type AppState = {
+  data: DateType | null;
+  report: RowData[];
+  total: number;
+  month: MonthKeyType;
+  year: string;
 };
 
-function App({ year = 2025 }: { year?: number; }) {
-  const [data, setData] = useState<DateType | null>(null);
-  const [report, setReport] = useState([]);
-  const [total, setTotal] = useState(0);
-  const [month, setMonth] = useState<MonthKeyType>('12');
+function App() {
+  const [state, setState] = useState<AppState>({
+    data: null,
+    report: [],
+    total: 0,
+    month: '12',
+    year: '2025',
+  });
   const getLastDayOfMonth = (year: number, month: number) => {
     return new Date(year, month, 0).getDate();
   };
@@ -34,66 +37,71 @@ function App({ year = 2025 }: { year?: number; }) {
     const dateStr = `${y}-${String(m + 1).padStart(2, '0')}-${String(d).padStart(2, '0')}`;
     const dayOfWeek = new Date(Date.UTC(y, m, d)).getUTCDay();
 
-    if (!data) {
+    if (!state.data) {
       return false;
     }
 
-    if (data.offDays.includes(dateStr)) {
+    if (state.data.offDays.includes(dateStr)) {
       return true;
     }
 
-    if ((dayOfWeek === 0 || dayOfWeek === 6) && !data.badDays.includes(dateStr)) {
+    if ((dayOfWeek === 0 || dayOfWeek === 6) && !state.data.badDays.includes(dateStr)) {
       return true;
     }
 
-    return data.holidays.includes(dateStr);
+    return state.data.holidays.includes(dateStr);
   });
 
   useEffect(() => {
+    let ignore = false;
+
     const fetchData = async () => {
       try {
-        const [countsResponse, reportResponse] = await Promise.all([
-          fetch('http://localhost:3000/api/counts'),
-          fetch('http://localhost:3000/api/reports'),
-        ]);
+        const { countsData, reportData } = await fetchDataFromApi(state.year);
 
-        if (!countsResponse.ok) {
-          throw new Error(`HTTP error fetching counts! status: ${countsResponse.status}`);
+        if (!ignore) {
+          setState((prev) => ({
+            ...prev,
+            data: countsData,
+            report: reportData.map((x, i) => ({
+              ...x,
+              id: i.toString(),
+              meta: { sort: true },
+            })),
+            total: reportData.reduce((a, x) => a + x.time, 0),
+          }));
         }
-        if (!reportResponse.ok) {
-          throw new Error(`HTTP error fetching reports! status: ${reportResponse.status}`);
+        } catch (error) {
+          console.error('There was a problem with the fetch operations:', error);
         }
-
-        const { data: countsData } = await countsResponse.json();
-        const { data: reportData } = await reportResponse.json();
-
-        setData(countsData);
-        setReport(reportData);
-        setTotal(reportData.reduce((a: number, x: { time: number }) => a + x.time, 0))
-      } catch (error) {
-        console.error('There was a problem with the fetch operations:', error);
-      }
-    };
+      };
 
     fetchData();
-  }, []);
 
-  const columns: TableColumnConfig<RowData>[] = [
-    { id: 'name', name: 'Наименование задачи', align: 'left', meta: { sort: true } },
-    { id: 'status', name: 'Статус задачи', align: 'center', meta: { sort: true } },
-    { id: 'time', name: 'Часы', align: 'center', meta: { sort: true } },
-  ];
+    return () => {
+      ignore = true; // Отменяем старый запрос при новом эффекте
+    };
+  }, [state.year]);
 
-  return (!data ? <Loader size="s" /> :
+  return (!state.data ? <Loader size="s" /> :
     <>
-      <Text variant="display-3">{year}</Text>
-      {data &&
+      <Text variant="display-3">{`Note ${state.year}`}</Text>
+        <div className={style.total}>
+          <Select
+            label="Year"
+            value={[state.year]}
+            className={style.select}
+            onUpdate={([t]) => setState((prev) => ({ ...prev, year: t as MonthKeyType }))}
+            options={years}
+          />
+        </div>
+      {state.data &&
         <div className="calendar">
           <div className={style.grid}>
-          {Object.keys(data.calendar).map((month) => {
+          {Object.keys(state.data.calendar).map((month) => {
             const monthNum = Number(month);
-            const minDate = dateTimeParse(new Date(`${year}-${monthNum}-01`));
-            const maxDate = dateTimeParse(new Date(`${year}-${monthNum}-${getLastDayOfMonth(year, monthNum)}`));
+            const minDate = dateTimeParse(new Date(`${state.year}-${monthNum}-01`));
+            const maxDate = dateTimeParse(new Date(`${state.year}-${monthNum}-${getLastDayOfMonth(Number(state.year), monthNum)}`));
 
             return (
               <div
@@ -112,36 +120,38 @@ function App({ year = 2025 }: { year?: number; }) {
         </div>
       </div>}
 
-      {data &&
+      {state.data &&
       <div className={style.total}>
         <Text variant="header-2">Details</Text>
         <Select
           label="Month"
-          value={[month]}
+          value={[state.month]}
           className={style.select}
-          onUpdate={([t]) => setMonth(t as MonthKeyType)}
+          onUpdate={([t]) => setState((prev) => ({ ...prev, month: t as MonthKeyType }))}
           options={options}
         />
         <div className={style.fields}>
-          {Object.entries(fields).map(([key, label]) => <TextInput
+          {Object.entries(fields).map(([key, label]) => 
+          <TextInput
             key={key}
             placeholder="0"
             label={label}
             disabled
-            value={`${data.calendar[month][key as KeyType]}`}
+            value={`${state?.data?.calendar[state.month][key as KeyType]}`}
           />)}
           <TextInput
             label="Сумма"
             disabled
             errorPlacement="inside"
             validationState="invalid"
-            value={`${total}`}
+            value={`${state.total}`}
           />
         </div>
       </div>}
-      <div className={style.total}>
+      <div className={style.report}>
+        <Text variant="header-2">Report</Text>
         <MyTable
-          data={report}
+          data={state.report}
           columns={columns}
         />
       </div>
