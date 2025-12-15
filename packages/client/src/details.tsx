2ba95e41f2937226253a395c90215bacd53a166e
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2025-12-15 21:17:26 +0300

    fix: detail

--- packages/client/src/details.tsx
+++ packages/client/src/details.tsx
@@ -1,45 +1,71 @@
-import { TextInput, Text, Select } from '@gravity-ui/uikit';
+import {
+  TextInput, Text, Select, Progress, ProgressColorStops,
+} from '@gravity-ui/uikit';
 import type { MonthKeyType, KeyType, DateType } from '@reports/shared';
 import { options, fields } from './constants';
+import { AppState } from './App';
 
 import './App.css';
 import style from './app.module.css';
-import { AppState } from './App';
 
 type DetailProps = {
   total: number;
+  issues: number;
   month: MonthKeyType;
   data: DateType;
   setState: (value: AppState | ((prevState: AppState) => AppState)) => void;
 }
 
-function Details({ month, total, data, setState }: DetailProps) {
+const colorStopsConfig: ProgressColorStops[] = [
+  { theme: 'danger', stop: 30 },
+  { theme: 'warning', stop: 60 },
+  { theme: 'success', stop: 100 }
+];
+
+function Details({ month, total, issues, data, setState }: DetailProps) {
   return <div className={style.total}>
     <Text variant="header-2">Details</Text>
-    <Select
-      label="Month"
-      value={[month]}
-      className={style.select}
-      onUpdate={([t]) => setState((prev: AppState) => ({ ...prev, month: t as MonthKeyType }))}
-  
-      options={options}
-    />
-    <div className={style.fields}>
-      {Object.entries(fields).map(([key, label]) => 
-      <TextInput
-        key={key}
-        placeholder="0"
-        label={label}
-        disabled
-        value={`${data?.calendar[month][key as KeyType]}`}
-      />)}
-      <TextInput
-        label="Total"
-        disabled
-        errorPlacement="inside"
-        validationState="invalid"
-        value={`${total}`}
+    <div className={style.progress}>
+      <Progress
+        value={total / data.calendar[month].hours * 100}
+        size="xs"
+        colorStops={colorStopsConfig}
       />
+    </div>
+    <div className={style.grid}>
+      <div className={style.fields}>
+        <Select
+          label="Month"
+          value={[month]}
+          onUpdate={([t]) => setState((prev: AppState) => ({ ...prev, month: t as MonthKeyType }))}
+          options={options}
+        />
+        {Object.entries(fields).map(([key, label]) => 
+          <TextInput
+            key={key}
+            placeholder="0"
+            label={label}
+            disabled
+            value={`${data?.calendar[month][key as KeyType]}`}
+          />)}
+      </div>
+
+      <div className={style.fields}>
+        <TextInput
+          label="Current total"
+          disabled
+          errorPlacement="inside"
+          validationState="invalid"
+          value={`${total}`}
+        />
+        <TextInput
+          label="Current issues"
+          disabled
+          errorPlacement="inside"
+          validationState="invalid"
+          value={`${issues}`}
+        />
+      </div>
     </div>
   </div>
 }
